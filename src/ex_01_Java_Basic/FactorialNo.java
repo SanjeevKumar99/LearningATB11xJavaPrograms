57da80ab48ca0e589023816295ffb29858955f9f
sanjeev <sanjeevskk99@gmail.com>
2025-03-07 16:22:03 +0530

    07/03- commited code.

--- src/ex_01_Java_Basic/FactorialNo.java
+++ src/ex_01_Java_Basic/FactorialNo.java
@@ -1,13 +1,13 @@
-public class FactorialNo {
+package ex_01_Java_Basic;
+
+public class FactorialNo{
     public static void main(String[] args) {
         int num=5;
         int fact=1;
         for(int i=1; i<=num; i++){
             fact= fact*i;
-
         }
         System.out.println(fact);
-
-
     }
 }
+
